60dd9a6b55b4717489eb290cce79fb4b16d2ce60
Marco <marco.odore@gmail.com>
2017-02-28 16:29:53 +0100

    added opening file to load data

--- data.c
+++ data.c
@@ -4,6 +4,12 @@
 
 void load_dataset(char *filename, dataset d_s)
 {
-  // to do
+  FILE *fp;
   printf("\nOpening File: %s\n", filename);
+  fp = fopen(filename, "r");
+  if (fp == NULL)
+  {
+    fprintf(stderr,"File %s could not be opened!\n", filename);
+  }
+  fprintf(fp, "Testing...\n");
 }
