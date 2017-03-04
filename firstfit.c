835e1d2994c1580abd6c98a04088109d1c85b125
Marco <marco.odore@gmail.com>
2017-03-04 18:13:22 +0100

    fixed minor bug

--- firstfit.c
+++ firstfit.c
@@ -13,6 +13,7 @@ void firstfit(dataset_t *ds, sol_t *sol)
       if(add_item_to_bin_if_fits(&(sol->bins[j]), ds->items[i]))
       {
         printf("bin j: %d, added item %d: %d\n",j, i, ds->items[i]);
+        printf("bin slack: %d\n", (sol->bins[j]).slack );
         fixed = TRUE;
         break;
       }
@@ -20,8 +21,9 @@ void firstfit(dataset_t *ds, sol_t *sol)
     if(fixed==FALSE && j==(sol->n))
     {
       add_new_bin(sol);
+      add_item_to_bin(&(sol->bins[(sol->n)-1]), ds->items[i]);
       printf("bin j: %d, added item %d: %d\n",j, i, ds->items[i]);
-      add_item_to_bin(&(sol->bins[sol->n]), ds->items[i]);
+      printf("bin slack: %d\n", (sol->bins[j]).slack );
     }
   }
   printf("Solution: %d\n", sol->n);
