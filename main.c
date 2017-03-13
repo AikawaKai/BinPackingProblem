2732ba097c8600cf11f06a1683a2be51b5098565
Marco <marco.odore@gmail.com>
2017-03-13 16:27:49 +0100

    loading and executing solution ok

--- main.c
+++ main.c
@@ -4,6 +4,50 @@
 #include "MBSprocedure.h"
 
 int main(int argc, char *argv[]){
+  char *filename = argv[1];
+  char buff[255];
+  int max_num_elem;
+  dataset_t *datasets;
+  sol_t *solutions;
+  FILE *filepointer;
+  filepointer = fopen(filename, "r");
+  if (filepointer == NULL)
+  {
+    fprintf(stderr,"File %s could not be opened!\n", filename);
+    exit(-1);
+  }
+  int num_cases;
+  fscanf(filepointer, "%s", buff);
+  num_cases = atoi(buff);
+  datasets = malloc(num_cases * sizeof(dataset_t));
+  solutions = malloc(num_cases * sizeof(sol_t));
+  for(int i=0; i<num_cases; i++)
+  {
+    load_dataset_from_file_pointer(filepointer, &datasets[i]);
+    max_num_elem = (datasets[i].bin_size / datasets[i].sorteditems[(datasets[i].n)-1])+1;
+    initialize_solution(&solutions[i], datasets[i].bin_size, datasets[i].n, max_num_elem);
+  }
+
+  for(int i=0; i<num_cases; i++)
+  {
+    printf("---------------------\n");
+    firstfit(&datasets[i], &solutions[i]);
+    printf("Solution firstfit: %d\n", solutions[i].n);
+    free_solution(&solutions[i]);
+    initialize_solution(&solutions[i], datasets[i].bin_size, datasets[i].n, max_num_elem);
+    firstfitdecreasing(&datasets[i], &solutions[i]);
+    printf("Solution firstfitdecreasing: %d\n", solutions[i].n);
+    free_solution(&solutions[i]);
+    initialize_solution(&solutions[i], datasets[i].bin_size, datasets[i].n, max_num_elem);
+    MBSmodified(&datasets[i], &solutions[i]);
+    printf("Solution MBSmodified: %d\n", solutions[i].n);
+  }
+
+
+
+
+
+  /*
   hashset_t test_best_set;
   hashset_t curre_a_set;
   dataset_t test1;
@@ -14,8 +58,7 @@ int main(int argc, char *argv[]){
   sol_t s2;
   sol_t s3;
   sol_t s4;
-  int max_num_elem;
-  char *filename = argv[1];
+
 
   load_dataset(filename, &test1); // carica il caso di test nella struct dataset
   load_dataset(filename, &test2);
@@ -41,5 +84,5 @@ int main(int argc, char *argv[]){
   free_solution(&s2); // libero spazio soluzione s2
   free_solution(&s3); // libero spazio soluzione s3
   free_solution(&s4); // libero spazio soluzione s4
-
+  */
 }
