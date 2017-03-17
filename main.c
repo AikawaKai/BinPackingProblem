d3608c49f702a4f02997e652c23caae25f3b2ec2
Marco <marco.odore@gmail.com>
2017-03-17 16:25:36 +0100

    updated

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]){
   char fileoutput[] = "output.csv";
   char buff[255];
   int max_num_elem;
-  int firstfit_res, firstfistdecr_res, mbs_res;
+  int firstfit_res, firstfistdecr_res, mbs_res, mbs_i_res;
   dataset_t *datasets;
   sol_t *solutions;
   FILE *filepointer;
@@ -30,9 +30,11 @@ int main(int argc, char *argv[]){
     max_num_elem = (datasets[i].bin_size / datasets[i].sorteditems[(datasets[i].n)-1])+1;
     initialize_solution(&solutions[i], datasets[i].bin_size, datasets[i].n, max_num_elem);
   }
+  //print_list(copy(datasets[0].head));
   filepointeroutput = fopen(fileoutput, "w");
   for(int i=0; i<num_cases; i++)
   {
+    node_t *new_head = malloc(sizeof(node_t));
     printf("---------------------\n");
     firstfit(&datasets[i], &solutions[i]);
     printf("Solution firstfit: %d\n", solutions[i].n);
@@ -44,14 +46,19 @@ int main(int argc, char *argv[]){
     printf("Solution firstfitdecreasing: %d\n", solutions[i].n);
     free_solution(&solutions[i]);
     initialize_solution(&solutions[i], datasets[i].bin_size, datasets[i].n, max_num_elem);
-    MBSmodified(&datasets[i], &solutions[i]);
+    new_head = copy(datasets[i].head);
+    MBS(&datasets[i], &solutions[i]);
     mbs_res = solutions[i].n;
+    printf("Solution MBS: %d\n", solutions[i].n);
+    free_list(datasets[i].head);
+    datasets[i].head = malloc(sizeof(node_t));
+    datasets[i].head = new_head;
+    free_solution(&solutions[i]);
+    initialize_solution(&solutions[i], datasets[i].bin_size, datasets[i].n, max_num_elem);
+    MBSmodified(&datasets[i], &solutions[i]);
+    mbs_i_res = solutions[i].n;
     printf("Solution MBSmodified: %d\n", solutions[i].n);
-    fprintf(filepointeroutput, "%d, %d, %d, %d\n", firstfit_res, firstfistdecr_res, mbs_res, datasets[i].best_sol);
-  }
-
-  for(int i=0; i<num_cases; i++)
-  {
+    fprintf(filepointeroutput, "%d, %d, %d, %d, %d\n", firstfit_res, firstfistdecr_res, mbs_res, mbs_i_res, datasets[i].best_sol);
     free_solution(&solutions[i]);
     free_dataset(&datasets[i]);
   }
