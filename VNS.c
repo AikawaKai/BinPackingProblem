d8613fb3b65645710646178d449531698aecd9e1
Marco <marco.odore@gmail.com>
2017-03-20 21:25:36 +0100

    udpated

--- VNS.c
+++ VNS.c
@@ -12,18 +12,24 @@ void VNSmethod(dataset_t *d_s, sol_t *starting_sol)
   int m_bins = starting_sol->n;
   int size_bin = 0;
   int bin_n;
+  int prec = 0;
+  int threshold = 0;
+  printf("--------\n");
   for(int i = 0; i<m_bins; i++)
   {
     bin_n = bins[i].n;
-    for(int j=0; j<bin_n;j++)
+    threshold = size_bin + threshold;
+    for(int j=prec; j<threshold;j++)
     {
       temp->id = i;
       temp->val = bins[i].items[j];
       temp->item_index_bin = j;
-      items[j+size_bin] = *temp;
+      items[i+j+size_bin-1] = *temp;
+      printf("%d\n", i+j+size_bin);
       temp = calloc(1, sizeof(node_t));
     }
-    size_bin= bins[i].n;
+    prec = threshold;
+    size_bin = bin_n;
   }
   //sort_not_linked_list(items, num_items);
   printf("###########\n");
