6f4695ecc29365d908c6a0131fdad31f76365b8c
Marco <marco.odore@gmail.com>
2017-04-01 15:18:39 +0200

    updated

--- move.c
+++ move.c
@@ -19,10 +19,22 @@ void performSwapMove(swap_t *transf, bin_t *bins)
 {
   node_t *item1 = transf->item1;
   node_t *item2 = transf->item2;
+  int bin_from = item1->id;
+  int bin_dest = item2->id;
+  float val1 = item1->val;
+  float val2 = item2->val;
   int index_in_bin_1 = item1->item_index_bin;
   int index_in_bin_2 = item2->item_index_bin;
-  removeItemFromBin(&bins[item1->id], index_in_bin_1);
-  removeItemFromBin(&bins[item2->id], index_in_bin_2);
+  removeItemFromBin(&bins[bin_from], index_in_bin_1);
+  removeItemFromBin(&bins[bin_dest], index_in_bin_2);
+  add_item_to_bin(&bins[bin_from], val2);
+  add_item_to_bin(&bins[bin_dest], val1);
+  item1->id = bin_dest;
+  item2->id = bin_from;
+  item1->val = val2;
+  item2->val = val1;
+  item1->item_index_bin = bins[bin_dest].n-1;
+  item2->item_index_bin = bins[bin_from].n-1;
 }
 
 void print_transfer_move(transfer_t *move)
